46c0c24f045a3a6b4f3d2954f96d37315fb0fae0
Ian A <ianazana.phi@gmail.com>
2025-08-03 13:12:00 +0800

    3D-Web-portfolio

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    card.style.setProperty('--x', `${e.clientX - rect.left}px`);
+    card.style.setProperty('--y', `${e.clientY - rect.top}px`);
+  });
+});
+
+document.getElementById('contact-btn').addEventListener('click', function () {
+    const email = "ianazana.phi@gmail.com";
+    const subject = "Let's Connect!";
+    const body = "Hi Ian,";
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoLink;
+});
+
+
+
+
